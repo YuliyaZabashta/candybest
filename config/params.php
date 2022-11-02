ec223a758d2638a34b193e043e55d604cb0490dd
Juliya <52375078+YuliyaZabashta@users.noreply.github.com>
2022-11-02 16:45:52 +0500

    Update params.php

--- config/params.php
+++ config/params.php
@@ -1,13 +1,13 @@
 <?php
 return[
-    'admin_email'=>'yuliyazabashta@yandex.ru',
+    'admin_email'=>'',
     'shop_name'=>'Магазин myproject.loc',
     'pagination'=>8,
     'smtp_host' => 'smtp.zoho.com',
     'smtp_port' => '465',
     'smtp_protocol' =>'ssl',
-    'smtp_login'=>'yuliyazabashta@yandex.ru',
-    'smtp_password'=>'Stan4210',
+    'smtp_login'=>'',
+    'smtp_password'=>'',
     'img_width' => 219,
     'img_height'=> 216,
     'gallery_width' => 700,
