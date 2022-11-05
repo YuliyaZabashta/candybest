817346f639741970eaa8d8de4e5e240e5b36dd80
Juliya <52375078+YuliyaZabashta@users.noreply.github.com>
2022-11-05 22:04:44 +0500

    Delete app/widgets directory

--- app/widgets/menu/menu_tpl/menu.php
+++
@@ -1,8 +0,0 @@
-<li>
-    <a href="?id=<?=$id;?>"><?=$category['title'];?></a>
-    <?php if(isset($category['child'])): ?>
-        <ul>
-            <?=$this->getMenuHtml($category['child']);?>
-        </ul>
-    <?php endif ?>
-</li>
